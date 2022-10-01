9aa403f86e0e22f32f1555c4b73a35f6dbc087f5
MD REJAUR RAHMAN <you@example.com>
2022-10-01 12:26:14 +0600

    Form created for Poll

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ const morgan = require('morgan');
 const app = express();
 // express function does 3 task (routing, middleware, template engine)
 
+app.set('view engine', 'ejs');
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/create', (req, res) => {
+  res.render('create');
+});
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+mongoose
+  .connect('mongodb://localhost:27017/test')
+  .then(() => {
+    app.listen(4545, () => {
+      console.log('Application is ready to serve on port 4545.');
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
